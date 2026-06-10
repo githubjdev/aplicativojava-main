7a4fc4c830e87bcbfb3946494619c43fc17b6071
alex_ <alex_@DESKTOP-CBITOJH>
2026-06-10 11:02:44 -0300

    Commit principal do projeto completo

---
+++ aplicativojava-main/src/main/java/br/com/integrator/exemplo/HomeController.java
@@ -0,0 +1,23 @@
+package br.com.integrator.exemplo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("titulo", "Exemplo Java com GiTHUB =)");
+        model.addAttribute("empresa", "Integrator Host do Brasil");
+        model.addAttribute("mensagem", "Aplicação Java com Spring Boot 4 e Thymeleaf funcionando com sucesso.");
+        model.addAttribute("dataHora", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+
+        return "index";
+
+    }
+}
